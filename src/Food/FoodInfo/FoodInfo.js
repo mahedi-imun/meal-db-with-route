712d04d06291ad6619425ce9c744e8a0c1b8670f
mahedi <mahedianik4@gmail.com>
2022-03-30 18:59:43 +0600

    add meal details components with route

--- src/Food/FoodInfo/FoodInfo.js
+++ src/Food/FoodInfo/FoodInfo.js
@@ -3,19 +3,25 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FoodInfo = () => {
-    const {foodId} = useParams();
-    const [meal , setMeal] = useState({})
-    useEffect(()=>{
+    const { foodId } = useParams();
+    const [meal, setMeal] = useState({})
+    useEffect(() => {
         const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`
         fetch(url)
-        .then(res => res.json())
-        .then(data => setMeal(data.meals[0]) )
-    },[foodId])
+            .then(res => res.json())
+            .then(data => setMeal(data.meals[0]))
+    }, [foodId])
     console.log(meal);
     return (
         <div>
-            <h4>food id: {foodId}</h4>
-            <h1>{meal.strMeal}</h1>
+            <a href="/" className=" mt-10 flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mx-auto">
+                <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={meal.strMealThumb} alt="" />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Name: {meal.strMeal}</h5>
+                    <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">category: {meal.strCategory}</h6>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{meal.strInstructions?.slice(1,180)}</p>
+                </div>
+            </a>
         </div>
     );
 };
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CustomLink from './CustomLink';
 
 const NavBar = () => {
     return (
 
         <nav className=' bg-violet-700 text-white flex justify-between items-center h-20 '>
-            <div>
-                <h4 className=' text-2xl pl-7'>Food || door</h4>
+            <div className=' text-2xl ml-5'>
+                <Link to="/">Food || door</Link>
             </div>
             <div className='flex '>
                 <CustomLink to="/">Home</CustomLink>
